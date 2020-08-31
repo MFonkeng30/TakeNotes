8253782bb1f0d37fa2f142dbfac9eebfb5825051
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-08-31 17:21:07 -0500

    added get routes for html

--- server.js
+++ server.js
@@ -7,3 +7,22 @@ var http = require("http");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// Test 
+// app.get("/", (request, response) => {
+//     response.send("Test!");
+// })
+
+// Get/notes - needs to return notes.html
+app.get("/notes", (request, response) => {
+
+    response.sendFile(path.join(__dirname, "public", "notes.html"));
+    console.log("Your Notes!");
+})
+
+// Get * - needs to return index.html (?)
+app.get("/", (request, response) => {
+    // response.sendFile(path.join(__dirname, "..", "..", "index.html"));
+    response.sendFile(path.join(__dirname, "public", "index.html"));
+    console.log("Your index!");
+})
