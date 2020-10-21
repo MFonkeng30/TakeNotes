26ed8e2f5861e5709602401f246b1453d7097c86
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-10-20 20:49:42 -0500

    changed paths of routes to match acting file paths

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
 //routes from server to user
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routing/apiroutes.js")(app);
+require("./routing/htmlroutes.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
